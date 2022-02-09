10061134401ddc9d26a9e85ec0090ce55cb37169
fanyanggod <fanyanggod95@gmail.com>
2022-02-09 17:47:55 +0800

    feat: ac no.26

---
+++ src/main/java/com/sherlock/leetcode/problem/no_26_remove_duplicates_from_sorted_array/Solution.java
@@ -0,0 +1,25 @@
+package com.sherlock.leetcode.problem.no_26_remove_duplicates_from_sorted_array;
+
+import java.util.Arrays;
+
+/**
+ * @author sherlock
+ * @date 2022/2/9
+ */
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+        int low = 1, fast = 1;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[fast-1]) {
+                nums[low] = nums[fast];
+                ++low;
+            }
+            ++fast;
+
+        }
+        return low;
+    }
+}
---
+++ src/test/java/com/sherlock/leetcode/problem/no_26_remove_duplicates_from_sorted_array/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.sherlock.leetcode.problem.no_26_remove_duplicates_from_sorted_array;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author sherlock
+ * @date 2022/2/9
+ */
+@Slf4j
+class SolutionTest {
+    Solution solution = new Solution();
+
+    @Test
+    void removeDuplicates() {
+        int result = solution.removeDuplicates(new int[]{1,1,2});
+        log.info(String.valueOf(result));
+    }
+}
