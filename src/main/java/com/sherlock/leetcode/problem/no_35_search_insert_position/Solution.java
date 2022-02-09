cf000d8da72fea4fb0f6ebc6aeadca25cea14c85
fanyanggod <fanyanggod95@gmail.com>
2022-02-09 18:24:26 +0800

    feat: ac no.35

---
+++ src/main/java/com/sherlock/leetcode/problem/no_35_search_insert_position/Solution.java
@@ -0,0 +1,24 @@
+package com.sherlock.leetcode.problem.no_35_search_insert_position;
+
+/**
+ * @author sherlock
+ * @date 2022/2/9
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return start;
+    }
+
+
+}
