fc3f07dbf7762e65e8bff4a3317c6d164f7adf71
sherlock <fanyanggod95@gmail.com>
2022-02-09 23:39:29 +0800

    feat: ac no.53

---
+++ src/main/java/com/sherlock/leetcode/problem/no_53_max_subarray/Solution.java
@@ -0,0 +1,16 @@
+package com.sherlock.leetcode.problem.no_53_max_subarray;
+
+/**
+ * @author sherlock
+ * @date 2022/2/9
+ */
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0, result= nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            result = Math.max(result, pre);
+        }
+        return result;
+    }
+}
