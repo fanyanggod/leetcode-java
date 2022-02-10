6732b112b2531369db41985d15c96e9fb57ddb0f
fanyanggod <fanyanggod95@gmail.com>
2022-02-10 10:01:28 +0800

    test: change test mode

--- src/test/java/com/sherlock/leetcode/problem/no_35_search_insert_position/SolutionTest.java
+++ src/test/java/com/sherlock/leetcode/problem/no_35_search_insert_position/SolutionTest.java
@@ -1,7 +1,12 @@
 package com.sherlock.leetcode.problem.no_35_search_insert_position;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,9 +18,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest {
     Solution solution = new Solution();
 
-    @Test
-    void searchInsert() {
-        int result = solution.searchInsert(new int[]{1}, 7);
-        log.info(String.valueOf(result));
+    @ParameterizedTest
+    @MethodSource("provideData")
+    void searchInsert(int[] nums,int target,int answer) {
+        int result = solution.searchInsert(nums, target);
+        assertEquals(answer, result, "Answer:" + answer + " ;Error result:" + result);
     }
-}
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(new int[]{1}, 7,1),
+                Arguments.of(new int[]{1,2,7,8}, 7,2)
+        );
+    }}
