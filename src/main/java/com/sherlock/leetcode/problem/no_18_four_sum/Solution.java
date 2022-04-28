bf46ca889ca886f94d50e7ffbe9ae04a4d5d1d7c
fanyanggod <fanyanggod95@gmail.com>
2022-04-28 14:09:30 +0800

    feat: ac no.18

---
+++ src/main/java/com/sherlock/leetcode/problem/no_18_four_sum/Solution.java
@@ -0,0 +1,53 @@
+package com.sherlock.leetcode.problem.no_18_four_sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author sherlock
+ */
+public class Solution {
+
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (nums.length < 4) {
+      return result;
+    }
+    Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length - 3; i++) {
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+
+      for (int j = i + 1; j < nums.length - 2; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) {
+          continue;
+        }
+        int left = j + 1;
+        int right = nums.length - 1;
+        while (left< right) {
+          if (nums[i] + nums[j] + nums[left] + nums[right] == target) {
+            result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+            right--;
+            left++;
+            while (left < right && nums[left] == nums[left-1]){
+              left++;
+            }
+            while (left < right && nums[right] == nums[right+1]){
+              right--;
+            }
+          } else if (nums[i] + nums[j] + nums[left] + nums[right] < target) {
+            left++;
+          } else {
+            right--;
+          }
+        }
+      }
+
+
+    }
+    return result;
+  }
+}
